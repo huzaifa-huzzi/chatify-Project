bdd071cb8ea7a133b344067c87c6283710790808
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-02-24 16:28:48 +0500

    Making Chat App

--- lib/View/chatPage/ChatPage.dart
+++ lib/View/chatPage/ChatPage.dart
@@ -1,6 +1,5 @@
 import 'package:chatify_app/Resources/Images/Images.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 
 class ChatPage extends StatefulWidget {
