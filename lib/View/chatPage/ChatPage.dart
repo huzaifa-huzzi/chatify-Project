ec10a4c2f02f0f5dbe4d2682c982d11f172856dd
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-02-25 20:37:23 +0500

    Making Chat App

--- lib/View/chatPage/ChatPage.dart
+++ lib/View/chatPage/ChatPage.dart
@@ -2,13 +2,14 @@ import 'package:chatify_app/Resources/Images/Images.dart';
 import 'package:flutter/material.dart';
 
 
+
+
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
-
 class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
