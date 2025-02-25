2014a8d5c43fb457cbad07fd40bed1d90c63f4ff
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-02-25 20:35:24 +0500

    Making Chat App

--- lib/Resources/Colors/Colors.dart
+++ lib/Resources/Colors/Colors.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
 
-   /// Light Colors
+   /// Light Theme Colors
   static const Color backgroundColor = Color(0xFF0A2A43);
   static const Color primaryColor = Color(0xFF0057FF);
   static const Color dContainerColor = Color(0xFFFF6F00);
