b71d6f42dca38fef1fd08db150664ac687fc1511
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-03-11 21:47:16 +0500

    Re-design the Widgets

--- lib/View/HomePage/HomeScreen.dart
+++ lib/View/HomePage/HomeScreen.dart
@@ -84,6 +84,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
       body:TabBarView(
         controller: tabController,
           children: [
+            ChatsList(),
             ListView(
               children: [
                 ListTile(
