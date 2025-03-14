f80dda74d95c975eb3c237e90997d0a07772eee4
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-03-14 22:04:27 +0500

    Screens Navigated to UserProfileScreen

--- lib/View/chatPage/ChatPage.dart
+++ lib/View/chatPage/ChatPage.dart
@@ -9,7 +9,6 @@ import 'package:chatify_app/View_Model/Controllers/chatController.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 
 class ChatPage extends StatefulWidget {
   final String userId;
