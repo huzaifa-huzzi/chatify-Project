bce837146bb45204b6535bcf4f6104d10adf2e1c
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-05-07 17:41:04 +0500

    Checking smooth working of the things

--- lib/View_Model/Controllers/ContactController.dart
+++ lib/View_Model/Controllers/ContactController.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:get/get.dart';
 
+
 class ContactController extends GetxController {
   RxBool isSearchEnabled = false.obs;
   final FirebaseAuth auth = FirebaseAuth.instance;
