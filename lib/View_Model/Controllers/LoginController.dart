6aa08c5a8ea47dfee45d39bed6003c0389cfa324
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-05-19 20:12:39 +0500

    Checking smooth working of the things

--- lib/View_Model/Controllers/LoginController.dart
+++ lib/View_Model/Controllers/LoginController.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 
+
 class LoginController extends GetxController {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final DatabaseReference _ref = FirebaseDatabase.instance.ref().child('user');
