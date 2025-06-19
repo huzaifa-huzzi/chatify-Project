db85ff70a2116e93aa97ce682274dd67a76b5134
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-06-19 22:12:16 +0500

    Checking smooth working of the things

--- lib/View/chatPage/Widgets/ChatsWidget.dart
+++ lib/View/chatPage/Widgets/ChatsWidget.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 
-
 class ChatsWidget extends StatelessWidget {
   final String message;
   final bool isComing;
